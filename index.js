05c7c37bc503b844e5085b209dc90bb74e957bc7
marcos-asdes <marco.engc@poli.ufrj.br>
2022-06-24 11:17:08 -0300

    build: added express, cors, pg and index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+import express, { json } from "express";
+import cors from "cors";
+//import dotenv from "dotenv";
+//dotenv.config();
+
+//import router from "./routes/index.js";
+
+const app = express();
+
+app.use(json());
+app.use(cors());
+//app.use(router);
+
+const port = /* process.env.PORT || */ 5000;
+app.listen(port, () => {
+  console.log(`Server is up on port: ${port}`);
+});

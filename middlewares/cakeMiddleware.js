b42fc8849176bae422e77cdadc988e8e5c44dab0
marcos-asdes <marco.engc@poli.ufrj.br>
2022-06-24 13:35:02 -0300

    feat: added cakeMiddleware.js

---
+++ middlewares/cakeMiddleware.js
@@ -0,0 +1,42 @@
+import { cakeSchema } from "../schemas/cakeSchema.js";
+import { cakesRepository }  from "../repositories/cakesRepository.js";
+
+export async function validateCake(req, res, next) {
+    const { name, price, description, image } = req.body
+    try {
+        const validate = cakeSchema.validate({ name, price, description, image }, { abortEarly: false });
+        if (validate.error) {
+            return res.status(400).send({
+                message: "Validation error",
+                detail: validate.error.details.map((e) => e.message).join(', ')
+            });
+        }
+        res.locals.name = name;
+        res.locals.price = price;
+        res.locals.description = description;
+        res.locals.image = image;
+        console.log("Validation parameters met");
+        next();
+    } catch (e) {
+        console.log("Error: " + e.message);
+        next();
+    }
+}
+
+export async function cakeAlreadyExist(req, res, next) {
+    try {
+        const { name } = req.body;
+        const checkCakeAlreadyExist = await cakesRepository.selectCakeByName(name);
+        if (checkCakeAlreadyExist.rows.length > 0) {
+            return res.status(409).send({
+                message: "Cake already registered",
+                detail: "Be sure to provide a valid cake recipe that does not match an already registered cake recipe",
+            });
+        }
+        console.log("No duplicate cake recipe records in the database");
+        next();
+    } catch (e) {
+        console.log("Error: " + e.message);
+        next();
+    }
+}
